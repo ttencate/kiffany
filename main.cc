86655e24a1e9ec5d02d2b54c59fb88f9f2efd03e
Thomas ten Cate <ttencate@gmail.com>
2011-09-24 18:39:45 +0100

    Add command line options for benchmarking

--- main.cc
+++ main.cc
@@ -4,14 +4,47 @@
 #include "terragen.h"
 #include "world.h"
 
+#include <boost/program_options.hpp>
+
 #include <iostream>
 #include <cstdlib>
+
+struct Flags {
+	bool help;
+	bool autofly;
+	bool vsync;
+	unsigned fixedTimestep;
+	unsigned exitAfter;
+};
+Flags flags;
 
 World *world = 0;
 Camera *camera = 0;
 
 int2 mousePos;
 bool running = true;
+
+bool parseCommandLine(int argc, char **argv, Flags &flags) {
+	namespace po = boost::program_options;
+
+	po::options_description desc("Available options");
+	desc.add_options()
+		("help", po::bool_switch(&flags.help), "print list of options and exit")
+		("autofly", po::bool_switch(&flags.autofly), "automatically fly forward")
+		("vsync", po::bool_switch(&flags.vsync), "synchronize on vertical blank")
+		("fixed_timestep", po::value<unsigned>(&flags.fixedTimestep)->default_value(0), "fixed simulation timestep value (ms)")
+		("exit_after", po::value<unsigned>(&flags.exitAfter)->default_value(0), "terminate after this many frames")
+	;
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc, argv, desc), vm);
+	po::notify(vm);
+
+	if (flags.help) {
+		std::cout << desc << '\n';
+		return false;
+	}
+	return true;
+}
 
 void keyCallback(int key, int state) {
 	if (state == GLFW_PRESS) {
@@ -24,14 +57,16 @@ void keyCallback(int key, int state) {
 }
 
 void mousePosCallback(int x, int y) {
-	int2 newMousePos(x, y);
-	int2 delta = newMousePos - mousePos;
+	if (!flags.autofly) {
+		int2 newMousePos(x, y);
+		int2 delta = newMousePos - mousePos;
 
-	float const s = 0.5f;
-	camera->setAzimuth(camera->getAzimuth() + s * -delta.x);
-	camera->setElevation(camera->getElevation() + s * -delta.y);
+		float const s = 0.5f;
+		camera->setAzimuth(camera->getAzimuth() + s * -delta.x);
+		camera->setElevation(camera->getElevation() + s * -delta.y);
 
-	mousePos = newMousePos;
+		mousePos = newMousePos;
+	}
 }
 
 void setup() {
@@ -44,27 +79,32 @@ void setup() {
 }
 
 void update(float dt) {
-	float const s = 30.0f * dt;
-	vec3 delta;
-	if (glfwGetKey('O')) {
-		delta += s * -X_AXIS;
+	if (flags.autofly) {
+		float const s = 30.0f * dt;
+		camera->moveRelative(s * Y_AXIS);
+	} else {
+		float const s = 30.0f * dt;
+		vec3 delta;
+		if (glfwGetKey('O')) {
+			delta += s * -X_AXIS;
+		}
+		if (glfwGetKey('U')) {
+			delta += s * X_AXIS;
+		}
+		if (glfwGetKey('E')) {
+			delta += s * -Y_AXIS;
+		}
+		if (glfwGetKey('.')) {
+			delta += s * Y_AXIS;
+		}
+		if (glfwGetKey(GLFW_KEY_LSHIFT)) {
+			delta += s * -Z_AXIS;
+		}
+		if (glfwGetKey(' ')) {
+			delta += s * Z_AXIS;
+		}
+		camera->moveRelative(delta);
 	}
-	if (glfwGetKey('U')) {
-		delta += s * X_AXIS;
-	}
-	if (glfwGetKey('E')) {
-		delta += s * -Y_AXIS;
-	}
-	if (glfwGetKey('.')) {
-		delta += s * Y_AXIS;
-	}
-	if (glfwGetKey(GLFW_KEY_LSHIFT)) {
-		delta += s * -Z_AXIS;
-	}
-	if (glfwGetKey(' ')) {
-		delta += s * Z_AXIS;
-	}
-	camera->moveRelative(delta);
 }
 
 void render() {
@@ -91,12 +131,16 @@ void render() {
 }
 
 int main(int argc, char **argv) {
+	if (!parseCommandLine(argc, argv, flags)) {
+		return 1;
+	}
+
 	if (!glfwInit()) {
 		return EXIT_FAILURE;
 	}
 
 	glfwOpenWindow(1024, 768, 8, 8, 8, 8, 16, 0, GLFW_WINDOW);
-	glfwSwapInterval(1); // vsync
+	glfwSwapInterval(flags.vsync ? 1 : 0);
 	glfwSetWindowTitle("Kiffany");
 
 	GLenum err = glewInit();
@@ -110,6 +154,7 @@ int main(int argc, char **argv) {
 	}
 
 	Camera camera;
+	camera.setPosition(vec3(0.0f, 0.0f, 16.0f));
 	::camera = &camera;
 	World world(&camera, new SineTerrainGenerator());
 	::world = &world;
@@ -126,15 +171,24 @@ int main(int argc, char **argv) {
 		timespec lastUpdate;
 		clock_gettime(CLOCK_MONOTONIC, &lastUpdate);
 		while (running && glfwGetWindowParam(GLFW_OPENED)) {
-			timespec now;
-			clock_gettime(CLOCK_MONOTONIC, &now);
-			float dt = (now.tv_sec - lastUpdate.tv_sec) + 1e-9 * (now.tv_nsec - lastUpdate.tv_nsec);
-			lastUpdate = now;
+			float dt;
+			if (flags.fixedTimestep) {
+				dt = 1e-3f * flags.fixedTimestep;
+			} else {
+				timespec now;
+				clock_gettime(CLOCK_MONOTONIC, &now);
+				dt = (now.tv_sec - lastUpdate.tv_sec) + 1e-9 * (now.tv_nsec - lastUpdate.tv_nsec);
+				lastUpdate = now;
+			}
 
 			update(dt);
 			render();
 			stats.framesRendered.increment();
 			glfwSwapBuffers();
+
+			if (flags.exitAfter && stats.framesRendered.get() >= flags.exitAfter) {
+				break;
+			}
 		}
 	}
 
