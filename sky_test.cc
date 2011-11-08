e7f7a86f9550ce12bd7d6201db746694f8c34b97
Thomas ten Cate <ttencate@gmail.com>
2011-11-08 23:04:17 +0000

    Make tests more comprehensive

--- sky_test.cc
+++ sky_test.cc
@@ -18,13 +18,17 @@ BOOST_AUTO_TEST_CASE(TestBuildOpticalLengthTable) {
 			uvec2 index = uvec2(layer, a);
 			double angle = opticalLengthTable.coordsFromIndex(index).y;
 			double opticalLength = opticalLengthTable.get(index);
+			// Optical length is never negative
 			BOOST_REQUIRE_LE(0, opticalLength);
-			if (a > 0 && angle < M_PI / 2) {
+			// Larger angles mean longer travel distance, hence larger optical length
+			if (a > 0) {
 				BOOST_REQUIRE_GE(opticalLength, opticalLengthTable.get(uvec2(layer, a - 1)));
 			}
-			if (angle < M_PI / 2  && layer > 0) {
+			// Higher layers mean shorter travel distance, hence smaller optical length
+			if (layer > 0) {
 				BOOST_REQUIRE_LE(opticalLength, opticalLengthTable.get(uvec2(layer - 1, a)));
 			}
+			// Angles beyond 90 degrees go through the earth, hence infinite optical length
 			if (angle >= M_PI / 2) {
 				BOOST_REQUIRE_EQUAL(std::numeric_limits<double>::infinity(), opticalLength);
 			}
@@ -43,13 +47,33 @@ BOOST_AUTO_TEST_CASE(TestBuildSunAttenuationTable) {
 	for (unsigned a = 0; a < numAngles; ++a) {
 		for (unsigned layer = 0; layer < numLayers; ++layer) {
 			uvec2 index = uvec2(layer, a);
+			double angle = sunAttenuationTable.coordsFromIndex(index).y;
 			dvec3 sunAttenuation = sunAttenuationTable.get(index);
+			// Attenuation factor should be 0 <= f <= 1
 			BOOST_REQUIRE_LE(0, sunAttenuation.r);
 			BOOST_REQUIRE_LE(0, sunAttenuation.g);
 			BOOST_REQUIRE_LE(0, sunAttenuation.b);
 			BOOST_REQUIRE_GE(1, sunAttenuation.r);
 			BOOST_REQUIRE_GE(1, sunAttenuation.g);
 			BOOST_REQUIRE_GE(1, sunAttenuation.b);
+			// Larger angles mean longer travel distance, hence smaller attenuation factor
+			if (a > 0) {
+				BOOST_REQUIRE_LE(sunAttenuation.r, sunAttenuationTable.get(uvec2(layer, a - 1)).r);
+				BOOST_REQUIRE_LE(sunAttenuation.g, sunAttenuationTable.get(uvec2(layer, a - 1)).g);
+				BOOST_REQUIRE_LE(sunAttenuation.b, sunAttenuationTable.get(uvec2(layer, a - 1)).b);
+			}
+			// Higher layers mean shorter travel distance, hence larger attenuation factor
+			if (layer > 0) {
+				BOOST_REQUIRE_GE(sunAttenuation.r, sunAttenuationTable.get(uvec2(layer - 1, a)).x);
+				BOOST_REQUIRE_GE(sunAttenuation.g, sunAttenuationTable.get(uvec2(layer - 1, a)).y);
+				BOOST_REQUIRE_GE(sunAttenuation.b, sunAttenuationTable.get(uvec2(layer - 1, a)).b);
+			}
+			// Angles beyond 90 degrees go through the earth, hence zero attenuation factor
+			if (angle >= M_PI / 2) {
+				BOOST_REQUIRE_EQUAL(0, sunAttenuation.r);
+				BOOST_REQUIRE_EQUAL(0, sunAttenuation.g);
+				BOOST_REQUIRE_EQUAL(0, sunAttenuation.b);
+			}
 		}
 	}
 }
