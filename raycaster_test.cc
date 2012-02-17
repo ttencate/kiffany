a8ad1397ca1a0ed591389db48134dac4f1485c34
Thomas ten Cate <ttencate@gmail.com>
2012-02-17 19:57:48 +0000

    Add stub raycaster test

---
+++ raycaster_test.cc
@@ -0,0 +1,17 @@
+#include "raycaster.h"
+#include "chunkmap.h"
+
+#include <boost/test/unit_test.hpp>
+
+namespace {
+	struct Fixture {
+
+	};
+}
+
+BOOST_FIXTURE_TEST_SUITE(RaycasterTest, Fixture)
+
+BOOST_AUTO_TEST_CASE(TestFoobar) {
+}
+
+BOOST_AUTO_TEST_SUITE_END()
